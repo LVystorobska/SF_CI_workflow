ca38b6bf2b7019f068ec5c0da49a2db88a791211
lolita.vystorobskaya@synebo.io <lolita.vystorobskaya@synebo.io>
2021-02-15 14:59:14 +0200

    build fixes

--- .github/sfdx-build.sh
+++ .github/sfdx-build.sh
@@ -1,11 +1,10 @@
         
-echo ${secrets.DEVHUB_TOKEN} > token.txt
 wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz
 mkdir sfdx-cli
 tar xJf sfdx-linux-amd64.tar.xz -C sfdx-cli --strip-components 1
 ./sfdx-cli/install
 echo 'Auth with the DevHub'
-sudo sfdx force:auth:sfdxurl:store -f token.txt -a DevHub -d
+sfdx force:auth:sfdxurl:store -f token.txt -a DevHub -d
 sfdx force:org:create -f config/project-scratch-def.json -a my-scratch
 sfdx force:source:push -u my-scratch
 sfdx force:org:list
